29c9c302bafc9e2881b680809d150ab0c29537be
Priyanshu Ahuja <26710303+Priyanshu-001@users.noreply.github.com>
2021-12-06 11:01:43 +0530

    sorta begning

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import MainPage from  './views/MainPage';
+import Navbar from './components/navbar';
+
+import 'primereact/resources/themes/tailwind-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar />
+        <MainPage/>
     </div>
+    </Router>
   );
 }
 
